2f71c0d6fafdfabd611e7cc29835b78233960836
Victor <victor@Nexus>
2016-05-04 13:47:58 -0400

    Created a *nix only way to save images for testing

---
+++ pictureTaker.py
@@ -0,0 +1,14 @@
+from subprocess import call
+from argparse import ArgumentParser
+
+def capture(fname, videoDevice='/dev/video1', resolution='1000x1000'):
+    call(['fswebcam', '-d', videoDevice, '-r', resolution, '--jpeg', '85', '--no-banner', fname])
+    return
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('fname', help='filename and path you wish to save to')
+    parser.add_argument('-d', '--device', default='/dev/video1', help='Path to image capture device')
+    parser.add_argument('-r', '--resolution', default='1000x1000', help='Image resolution in pixels')
+    args = parser.parse_args()
+    capture(args.fname, args.device, args.resolution)
