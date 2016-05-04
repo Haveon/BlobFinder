a00433de7e9551605d50a70f933d13d07a748af4
Victor <victor@Nexus>
2016-05-04 16:26:09 -0400

    Tweaked the resolution to the camera's max

--- pictureTaker.py
+++ pictureTaker.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('fname', help='filename and path you wish to save to')
     parser.add_argument('-d', '--device', default='/dev/video1', help='Path to image capture device')
-    parser.add_argument('-r', '--resolution', default='1000x1000', help='Image resolution in pixels')
+    parser.add_argument('-r', '--resolution', default='1920x1080', help='Image resolution in pixels')
     args = parser.parse_args()
     capture(args.fname, args.device, args.resolution)
