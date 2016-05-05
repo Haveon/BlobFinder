a1341bdd8a7709e82d318289e81f93cdc457d3cf
Victor Reyes <vreyesos@uwaterloo.ca>
2016-05-05 15:08:57 -0400

    blobFinder Take 2. Complete refactor of first. Added a way to perhaps be able to find the cup handle

--- blobFinder.py
+++ blobFinder.py
@@ -1,50 +1,95 @@
-from scipy.ndimage import imread, label, center_of_mass, gaussian_filter
+from scipy.ndimage import imread, label, center_of_mass, gaussian_filter, sobel
+from numpy import nonzero, argmax, arctan2
 from argparse import ArgumentParser
 from matplotlib import pyplot as plt
 
-def imLoad(fname):
-    """
-    Given the filename of an image, the function will load it into memory and
-    grayscale it.
-    """
-    img = imread(fname)
-    r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
-    gray = 0.2989*r + 0.5870*g + 0.1140*b
-    return gray
+class Blob:
+    def __init__(self, x, y, farX, farY):
+        self.x, self.y = x, y
+        self._fx, self._fy = farX, farY
 
-def findBlob(tgtImg, refImg, filterSigma=39, threshold=60):
-    """
-    Given a target Image and a reference Image (background), finds the center
-    of mass of a the one blob in the image.
-    Returns the coordinates of the blob's center of mass
-    """
-    img = abs(tgtImg - refImg)              # Remove background
-    img[img<threshold] = 0                  # Truncate low intensities to zero
-    img = gaussian_filter(img,filterSigma)  # Filter out some noise    
+        self.orientationVector = farX - x, farY - y
+        self.orientation = arctan2(farY-y, farX-x)
 
-    labeledArray, numFeatures = label(img)
-    plt.subplot(221)
-    plt.imshow(img)
-    plt.subplot(223)
-    plt.imshow(labeledArray)
-    plt.show()
-    assert 0<numFeatures<2, "There are {} recognized objects, there can only be one".format(numFeatures)
+    def getLocation(self):
+        return self.x, self.y
 
-    coord = center_of_mass(labeledArray)
-    return coord
+    def getOrientation(self):
+        return self.orientation
 
-def blobCoord(fnameTgt, fnameRef, filterSigma=39, threshold=60):
-    tgtImg = imLoad(fnameTgt)
-    refImg = imLoad(fnameRef)
-    coord = findBlob(tgtImg, refImg, filterSigma, threshold)
-    return coord
+    def __str__(self):
+        return 'Center: {}, Orientation: {}'.format(self.getLocation(),self.getOrientation())
+
+class BlobFinder:
+    def __init__(self, tgtFname, refFname):
+        tgtImg = self._imLoad(tgtFname)
+        refImg = self._imLoad(refFname)
+        self.image = abs(tgtImg - refImg)
+
+    def _imLoad(self, fname):
+        """
+        Given the filename of an image, the function will load it into memory and
+        grayscale it.
+        """
+        img = imread(fname)
+        r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
+        gray = 0.2989*r + 0.5870*g + 0.1140*b
+        return gray
+
+    def findBlob(self, cutoff=100, sigma=3):
+        self.highPassImage = self._highPass(self.image, cutoff)
+        self.filterImage   = self._filter(self.highPassImage, sigma)
+        self.labeledImage, numFeatures = self._label(self.filterImage)
+
+        assert 0<numFeatures<2, "There are {} recognized objects, there can only be one".format(numFeatures)
+        y, x = self._findCenter(self.labeledImage)
+        farY, farX  = self._findFarEdge(self.labeledImage)
+        blob = Blob(x, y, farX, farY)
+        return blob
+
+    def _highPass(self, image, cutoff):
+        hpImage = image.copy()
+        hpImage[hpImage<cutoff] = 0
+        return hpImage
+
+    def _filter(self, image, sigma):
+        gfImage = gaussian_filter(image, sigma)
+        return gfImage
+
+    def _label(self, image):
+        return label(image)
+
+    def _findCenter(self, labeledImage):
+        return center_of_mass(labeledImage)
+
+    def _findEdges(self, image):
+        simg = sobel(image)
+        simg[simg!=0] = 1
+        return simg
+
+    def _2dNorm(self, x0, y0, x1, y1):
+        return ((x0 - x1)**2 + (y0 - y1)**2)**0.5
+
+    def _findFarEdge(self, labeledImage):
+        center = self._findCenter(labeledImage)
+        edgeImg= self._findEdges(labeledImage)
+
+        ys, xs = nonzero(edgeImg)
+        distancesFromCenter= self._2dNorm(center[1], center[0], ys, xs)
+
+        indFarthest = argmax(distancesFromCenter)
+        farY, farX = ys[indFarthest], xs[indFarthest]
+        return farY, farX
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('tgtFname', help='Filepath to target image')
     parser.add_argument('refFname', help='Filepath to reference image')
-    parser.add_argument('--filterSigma', default=9, type=int, help='Value of Sigma in gaussian_filter')
-    parser.add_argument('--threshold', default=65, type=int, help='Truncation Threshold')
+    parser.add_argument('--sigma', default=3, type=int, help='Value of Sigma in gaussian_filter')
+    parser.add_argument('--cutoff', default=100, type=int, help='Truncation Threshold')
 
     args = parser.parse_args()
-    print blobCoord(args.tgtFname, args.refFname, args.filterSigma, args.threshold)
+    finder = BlobFinder(args.tgtFname, args.refFname)
+    blob = finder.findBlob(cutoff=args.cutoff, sigma=args.sigma)
+    print blob
