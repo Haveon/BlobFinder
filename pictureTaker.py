a2f8b563d971ee7984824ac3708337ac1dcdb42d
Victor <victor@Nexus>
2016-05-13 12:04:44 -0400

    Added the ability to take multiple sequential pictures Helps calibration

--- pictureTaker.py
+++ pictureTaker.py
@@ -10,5 +10,12 @@ if __name__ == '__main__':
     parser.add_argument('fname', help='filename and path you wish to save to')
     parser.add_argument('-d', '--device', default='/dev/video1', help='Path to image capture device')
     parser.add_argument('-r', '--resolution', default='1920x1080', help='Image resolution in pixels')
+    parser.add_argument('--repeat', type=int, default='1', help='How many pictures to take')
     args = parser.parse_args()
-    capture(args.fname, args.device, args.resolution)
+    if args.repeat!=1:
+        fname,ext = args.fname.split('.')
+        for i in range(args.repeat):
+            raw_input('Press Enter for next picture')
+            capture(fname+str(i)+'.'+ext, args.device, args.resolution)
+    else:
+        capture(args.fname, args.device, args.resolution)
